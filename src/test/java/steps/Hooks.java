21b1bdce05797c58726ff930b7ad346b88506f41
Jose Salazar <jose.salazar@nolatech.ai>
2024-10-14 14:43:42 -0400

    delete de imports que no se usan

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -10,16 +10,10 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.http.ContentType;
 
 import java.time.Duration;
 import java.util.*;
