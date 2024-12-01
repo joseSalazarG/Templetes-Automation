22f7dc0e04bdad387c1ac97efca58d936d8d669e
Jose Salazar <jose.salazar@nolatech.ai>
2024-11-30 23:14:47 -0400

    test 4

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,32 +1,24 @@
 package steps;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.qameta.allure.Step;
 import pom.login.LoginLogic;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
 
 public class LoginSteps {
 
     LoginLogic loginLogic = new LoginLogic();
 
-    @Step("Abro la pagina web")
     @Given("Navego a la pagina web")
     public void navegoPaginaWeb() {
         loginLogic.navegoPaginaWeb();
     }
 
-    @Step("Ingreso el correo y la contraseña")
     @When("Ingreso el correo {string} y la contraseña {string}")
     public void ingresoElCorreoYLaContrasena(String correo, String password) {
         loginLogic.ingresoCorreoYContrasena(correo, password);
     }
 
-    @Step("El coco")
-    @Severity(SeverityLevel.CRITICAL)
     @Then("Valido que me logueo de forma exitosa")
     public void validoQueMeLogueoDeFormaExitosa() {
         loginLogic.validoLoginExitoso();
